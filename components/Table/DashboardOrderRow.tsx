bbc7c09f826d9ef09bcba2aa30243331a9b33b29
Haroun Ach <harounacho21@gmail.com>
2023-02-27 14:32:59 +0100

    Fix Admin dashboard orders

--- components/Table/DashboardOrderRow.tsx
+++ components/Table/DashboardOrderRow.tsx
@@ -2,26 +2,43 @@ import React from "react";
 import IconButton from "@/components/Button/IconButton";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import Order from "@/types/Order";
+import { formatFriendyDate } from "@/lib/dateUtils";
 
 interface DashboardOrderRowProps {
   order: Order;
 }
 
 const DashboardOrderRow = ({ order }: DashboardOrderRowProps) => {
+  const paidText = order.isPaid ? formatFriendyDate(order.paidAt) : "Not Paid";
+  const deliverText = order.isDelivered
+    ? formatFriendyDate(order.deliveredAt)
+    : "Not Delivered";
+
+  const redtext = (value: boolean) => (!value ? "text-red-500" : "");
   return (
     <tr>
       <td className="border-2 border-gray-200 px-4">{order._id}</td>
-      <td className="border-2 border-gray-200 px-4">12 Dec 2022</td>
+      <td className="border-2 border-gray-200 px-4">
+        {formatFriendyDate(order.createdAt)}
+      </td>
       <td className="border-2 border-gray-200 px-4">{`$${order.totalPrice}`}</td>
-      <td className="border-2 border-gray-200 px-4">12 Dec 2022</td>
-      <td className="border-2 border-gray-200 px-4">12 Dec 2022</td>
+      <td className={`border-2 border-gray-200 px-4 ${redtext(order.isPaid)}`}>
+        {paidText}
+      </td>
+      <td
+        className={`border-2 border-gray-200 px-4 ${redtext(
+          order.isDelivered
+        )}`}
+      >
+        {deliverText}
+      </td>
       <td className="border-2 border-gray-200 px-4">
         <div className="flex items-center justify-center">
           <IconButton
             icon={faEye}
             variant="primaryIcon"
             size="normal"
-            url={`/orders/${order._id}`}
+            url={`/admin/orders/${order._id}`}
           />
         </div>
       </td>
