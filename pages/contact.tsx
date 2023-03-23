b65e615bea5aa415e690acee7ba8fe716811fe8a
Haroun Ach <harounacho21@gmail.com>
2023-03-23 18:44:00 +0100

    Add about and contact pages content

---
+++ pages/contact.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/components/Layout/Layout";
+
+export default function Contact() {
+  return (
+    <Layout>
+      <section className="container mx-auto mt-6">
+        <h1 className="mb-4 text-center text-2xl">Contact</h1>
+        <p className="mb-14 text-center text-base text-neutral-500">
+          Have a question? Get in Touch with us.
+        </p>
+        <div className="mb-6 grid grid-cols-5 gap-6"></div>
+      </section>
+    </Layout>
+  );
+}
