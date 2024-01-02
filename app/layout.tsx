818874ac2fa3859d0d21e1f4c2df614a66670e94
Haroun Ach <harounacho21@gmail.com>
2024-01-03 00:09:02 +0100

    Apply styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
+import "@/app/ui/styles/main.scss";
+
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
