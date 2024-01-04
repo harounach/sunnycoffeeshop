bf242b3e860cd5790e74979300c9c7f978b70aed
Haroun Ach <harounacho21@gmail.com>
2024-01-04 02:33:50 +0100

    Create IconButton component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Button from "./ui/actionables/buttons/Button";
+import IconButton from "./ui/actionables/buttons/IconButton";
+import { AlarmFill, Facebook, CartFill } from "react-bootstrap-icons";
 
 export default function Page() {
   return (
@@ -7,6 +9,24 @@ export default function Page() {
       <Button label="Button" />
       <Button variant="danger" label="Button" />
       <Button variant="neutral" label="Button" />
+      <IconButton>
+        <CartFill title="Activity" />
+      </IconButton>
+      <IconButton color="primary">
+        <Facebook title="Activity" />
+      </IconButton>
+      <IconButton color="danger">
+        <Facebook title="Activity" />
+      </IconButton>
+      <IconButton color="primary" hasBG>
+        <Facebook title="Activity" />
+      </IconButton>
+      <IconButton color="danger" hasBG>
+        <Facebook title="Activity" />
+      </IconButton>
+      <IconButton mini>
+        <Facebook title="Activity" />
+      </IconButton>
     </div>
   );
 }
---
+++ app/ui/actionables/buttons/IconButton.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import classNames from "classnames";
+
+interface IconButtonProps {
+  url?: string;
+  color?: "primary" | "danger";
+  hasBG?: boolean;
+  mini?: boolean;
+  customeClasses?: string;
+  children: React.ReactNode;
+}
+
+export default function IconButton({
+  url,
+  color,
+  hasBG,
+  mini,
+  customeClasses,
+  children,
+}: IconButtonProps) {
+  const classes = classNames(
+    "icon-btn",
+    { "icon-btn--primary": color === "primary" },
+    { "icon-btn--danger": color === "danger" },
+    { "icon-btn--bg": hasBG },
+    { "icon-btn--mini": mini },
+    customeClasses,
+  );
+
+  // If there is a "url" prop we want to the button to be a link
+  if (url) {
+    return (
+      <Link className={classes} href={url}>
+        {children}
+      </Link>
+    );
+  }
+
+  // otherwise we just return button element
+  return <button className={classes}>{children}</button>;
+}
